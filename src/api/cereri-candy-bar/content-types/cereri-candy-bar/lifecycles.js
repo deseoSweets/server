b7b13d4a8d8e6e2fb7638d5dc08884f3236941a6
Victor <91851706+ashcsh@users.noreply.github.com>
2023-10-19 20:53:14 +0300

    tranzactii

--- src/api/cereri-candy-bar/content-types/cereri-candy-bar/lifecycles.js
+++ src/api/cereri-candy-bar/content-types/cereri-candy-bar/lifecycles.js
@@ -8,20 +8,22 @@ module.exports = {
 
     async afterCreate(event) {
         const { result, params } = event
-        try {
+        if (result.adresaEmail) {
+            try {
 
-            await mail.send({
-                from: 'Comenzi Insightweb <noreplay@insightweb.ro>',
-                replyTo: 'comezni@insightweb.ro',
-                templateId: "d-15e06de3b9944bb09ca95b9313079a18",
-                personalizations: [
-                    {
-                        to: `${result.adresaEmail}`,
-                    }
-                ]
-            })
-        } catch (error) {
-            console.log(error)
+                await mail.send({
+                    from: 'Comenzi Insightweb <noreplay@insightweb.ro>',
+                    replyTo: 'comezni@insightweb.ro',
+                    templateId: "d-15e06de3b9944bb09ca95b9313079a18",
+                    personalizations: [
+                        {
+                            to: `${result.adresaEmail}`,
+                        }
+                    ]
+                })
+            } catch (error) {
+                console.log(error)
+            }
         }
     },
 }
